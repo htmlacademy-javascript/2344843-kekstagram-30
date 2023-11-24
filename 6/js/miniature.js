ea7c039cbbb1e43d25569d8a75e49026c21ba6aa
Keks <keks@htmlacademy.ru>
2023-11-24 16:06:01 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/miniature.js
@@ -0,0 +1,18 @@
+const renderPictures = (data) => {
+  const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const picturesContainer = document.querySelector('.pictures');
+  const fragment = document.createDocumentFragment();
+
+  data.forEach((picture) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__img').alt = picture.description;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+    fragment.appendChild(pictureElement);
+  });
+
+  picturesContainer.appendChild(fragment);
+};
+
+export {renderPictures};
---
+++ 6/js/util.js
@@ -0,0 +1,26 @@
+// Функция для генерации случайного числа в заданном диапазоне
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Функция для выбора случайного элемента из массива
+const getRandomElement = (array) => array[getRandomInt(0, array.length - 1)];
+
+function getId (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInt(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      window.console.error(`Перебраны все числа из диапазона от ${ min } до ${ max}`);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInt(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+export {getRandomElement, getId, getRandomInt};
