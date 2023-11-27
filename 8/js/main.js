24c23f33206008ee0b83ffb6db7ef9eee79c7e58
Keks <keks@htmlacademy.ru>
2023-11-27 18:31:32 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import {photos} from './data.js';
+import {renderPictures} from './miniature.js';
+import './big-pucture.js';
+// Вывод массива фотографий
+window.console.log(photos);
+
+renderPictures(photos);
